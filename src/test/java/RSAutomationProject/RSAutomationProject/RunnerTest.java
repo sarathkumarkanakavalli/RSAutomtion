114ef5bd2438ca77e934d888e6f563f982b94c98
sarathkumarkanakavalli <36608055+sarathkumarkanakavalli@users.noreply.github.com>
2018-02-19 13:09:29 +0530

    Update RunnerTest.java

--- src/test/java/RSAutomationProject/RSAutomationProject/RunnerTest.java
+++ src/test/java/RSAutomationProject/RSAutomationProject/RunnerTest.java
@@ -7,7 +7,9 @@ import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src\\test\\resources" }, format = {"json:target/CucumberTestReport.json","html:target/site/cucumber-pretty"}, glue ="RSAutomationProject\\RSAutomationProject")
-public class RunnerTest extends AbstractTestNGCucumberTests{
-
+@CucumberOptions(features = { "src\\test\\resources" }, format = {
+		"json:target/CucumberTestReport.json",
+		"html:target/site/cucumber-pretty" }, glue = "RSAutomationProject\\RSAutomationProject",
+		tags = {"@EndtoEnd,@SearchResults"})
+public class RunnerTest extends AbstractTestNGCucumberTests {
 }
